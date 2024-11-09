e4d45c52f1f58f642c3235928888cb333005ef8e
Connaker <mconnaker@gmail.com>
2024-11-09 07:33:50 -0600

    Changing code back.

--- weatherapp/app.py
+++ weatherapp/app.py
@@ -1,11 +1,11 @@
-
 from pyowm import OWM
 from pyowm.commons.exceptions import NotFoundError
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 from flask_wtf.csrf import CSRFProtect
 from pyzipcode import ZipCodeDatabase
 import ast
 import os
+
 
 SECRET_KEY = os.urandom(32)
 app = Flask(__name__)
@@ -17,13 +17,9 @@ owm = OWM(os.environ['API'])
 def index():
     return render_template('weather.html')
 
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template("500.html")
 
 @app.route('/search', methods=['POST'])
 def search():
-    weather = {}
     search = request.form['response']
     if search == '':
         e = 'Field cannot be blank'
@@ -35,29 +31,44 @@ def search():
             city = zcdb[n].city
             state = zcdb[n].state
             place = city+','+state+','+"US"
-            weather.update({
-                    'zipcode': n,
-                    'city': city,
-                    'state': state,
-                    'place': place,
-            })
+            mgr = owm.weather_manager()
+            observation = mgr.weather_at_place(place)
+            w = observation.weather
+            ftemp = w.temperature(unit='fahrenheit')['temp']
+            ctemp = w.temperature(unit='celsius')['temp']
+            descript = w.detailed_status
+
+            weather = {
+                'zipcode': n,
+                'current': descript,
+                'city': city,
+                'state': state,
+                'place': place,
+                'ftemp': ftemp,
+                'ctemp': ctemp,
+            }
+            return render_template("zip.html", weather=weather)
         else:
             e = 'Field is invalid Zip'
             return render_template('invalid.html', error=e)
-    if place is None:
-        place = search
-        weather.update ({
-            'location': place,
-        })
-    mgr = owm.weather_manager()
-    observation = mgr.weather_at_place(place)
-    w = observation.weather
-    ftemp = w.temperature(unit='fahrenheit')['temp']
-    ctemp = w.temperature(unit='celsius')['temp']
-    descript = w.detailed_status
-    weather.update ({
-        'current': descript,
-        'ftemp': ftemp,
-        'ctemp': ctemp,
-    })
-    return render_template("zip.html", weather=weather)
+    else:
+        try:
+
+            location = ast.literal_eval(search)
+            mgr = owm.weather_manager()
+            observation = mgr.weather_at_place(location)
+            w = observation.weather
+            ftemp = w.temperature(unit='fahrenheit')['temp']
+            ctemp = w.temperature(unit='celsius')['temp']
+            descript = w.detailed_status
+            weather = {
+                'location': location,
+                'current': descript,
+                'ftemp': ftemp,
+                'ctemp': ctemp,
+            }
+            return render_template("city.html", weather=weather)
+
+        except NotFoundError:
+            e = search, '+', 'not in regsitry. Please check your input'
+            return render_template("invalid.html", error=e)
